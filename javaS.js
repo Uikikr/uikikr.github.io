050c7c84ec6e3149779b9fea127e5c9a1db9f186
Fredrik Trusohamn <48571579+Uikikr@users.noreply.github.com>
2024-04-30 08:26:55 +0300

    ep61

--- javaS.js
+++ javaS.js
@@ -246,3 +246,27 @@
   </div>`;
   };
 })();
+var refs = document.querySelectorAll(".referens");
+function formatUrl(url) {
+  // Split the URL into an array to distingish double slashes from single slashes
+  var doubleSlash = url.split("//");
+
+  // Format the strings on either side of double slashes separately
+  var formatted = doubleSlash
+    .map(
+      (str) =>
+        // Insert a word break opporunity after a colon
+        str
+          .replace(/(?<after>:)/giu, "$1<wbr>")
+          // Before a single slash, tilde, period, comma, hyphen, underline, question mark, number sign, or percent symbol
+          .replace(/(?<before>[/~.,\-_?#%])/giu, "<wbr>$1")
+          // Before and after an equals sign or ampersand
+          .replace(/(?<equals>=)/giu, "<wbr>$1<wbr>")
+          .replace(/(?<ampersand>&amp;)/giu, "<wbr>&<wbr>")
+
+      // Reconnect the strings with word break opportunities after double slashes
+    )
+    .join("//<wbr>");
+
+  return formatted;
+}
